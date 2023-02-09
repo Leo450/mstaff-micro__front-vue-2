14bbc03de474df072120187b87922523612b36ca
Leo450 <leo450450@gmail.com>
2023-02-09 13:21:44 +0100

    button + submodule update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,18 @@
 <template>
 	<div class="home">
+
 		<Header/>
+
+		<b-container class="mt-5">
+			<b-row align-h="center">
+				<b-col cols="auto">
+
+					<b-button variant="info">qzd</b-button>
+
+				</b-col>
+			</b-row>
+		</b-container>
+
 	</div>
 </template>
 
