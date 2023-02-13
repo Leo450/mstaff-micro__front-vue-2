f89a390b99c3b0976ca188f38ad2aefdbb282c27
Leo450 <leo450450@gmail.com>
2023-02-13 23:06:27 +0100

    docker port

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: 'http://localhost:21000/',
+  publicPath: 'http://localhost:21001/',
   transpileDependencies: true,
   devServer: {
     allowedHosts: 'all'
